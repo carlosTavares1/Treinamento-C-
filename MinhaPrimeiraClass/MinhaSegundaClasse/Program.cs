0e0e3c07006f8a4d270d24a863e038c5054e640a
Carlos Tavares <cast.3@hotmail.com>
2019-09-18 17:31:01 -0300

    Usando o model e criando um class

---
+++ MinhaPrimeiraClass/MinhaSegundaClasse/Program.cs
@@ -0,0 +1,37 @@
+﻿using MinhaSegundaClasse.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhaSegundaClasse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var minhaCestaDeFrutas = new List<Frutas>();
+
+            minhaCestaDeFrutas.Add(new Frutas()
+            {
+                nome = "Banana",
+                quantidade = 15
+            });
+
+            minhaCestaDeFrutas.Add(new Frutas()
+            {
+                nome = "Maçã",
+                quantidade = 8
+            });
+
+            minhaCestaDeFrutas.Add(new Frutas()
+            {
+                nome = "Jaca",
+                quantidade = 7
+            });
+            minhaCestaDeFrutas.ForEach(i => Console.WriteLine($"Nome:{i.nome} Quantidade:{i.quantidade}"));
+            Console.ReadKey();
+        }
+    }
+}
