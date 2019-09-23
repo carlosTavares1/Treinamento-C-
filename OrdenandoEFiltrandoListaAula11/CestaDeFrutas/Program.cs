7324b351512ef6e8eab865579ec04b815584a948
Carlos Tavares <cast.3@hotmail.com>
2019-09-23 17:31:52 -0300

    Ordenandolista e filtrando lista usando where e findall

--- OrdenandoEFiltrandoListaAula11/CestaDeFrutas/Program.cs
+++ OrdenandoEFiltrandoListaAula11/CestaDeFrutas/Program.cs
@@ -12,6 +12,7 @@ namespace CestaDeFrutas
         {
             var cestaDeFrutas = new List<Fruta>();
             //vamos no mercado
+            #region Carregando minha cesta
             //Tomate
             cestaDeFrutas.Add(new Fruta()
             {
@@ -36,19 +37,21 @@ namespace CestaDeFrutas
                 Cor = "Amarelo",
                 Peso = 150
             });
-
+            #endregion
+            //Listas
+            #region Lista 1
             cestaDeFrutas.
                 //Neste ponto ordenamos os valores de forma decrescente pelo nome
                 OrderByDescending(x => x.Id).ToList<Fruta>().ForEach(i => Console.WriteLine($"Id: {i.Id} | Nome: {i.Nome}"));
-
+            #endregion
             Console.WriteLine("-------------------------------------");
-
+            #region Lista 2
             cestaDeFrutas.
                 //Aqui ordenamos os valores pelo id de forma crescente
                 OrderBy(x => x.Id).ToList<Fruta>().ForEach(i => Console.WriteLine($"Id: {i.Id} | Nome: {i.Nome}"));
-
+            #endregion
             Console.WriteLine("-------------------------------------Filtro Peso");
-
+            #region Lista 3
             var filtroCesta = cestaDeFrutas.
                 //Aqui filtramos os registros maiores de 100 gramas
                 Where(x => x.Peso > 100).OrderBy(x => x.Nome);
@@ -56,6 +59,29 @@ namespace CestaDeFrutas
             filtroCesta.ToList<Fruta>().ForEach(i => Console.WriteLine($"Id {i.Id} Nome {i.Nome} Peso {i.Peso}"));
             //frutinha recebe cada fruta da nossa cesta
             (from frutinha in cestaDeFrutas where frutinha.Nome == "Manga" select frutinha).ToList<Fruta>().ForEach(i => Console.WriteLine($"Fruta escolhida {i.Nome}"));
+            #endregion
+            Console.WriteLine("-------------------------------------Filtro Cor");
+
+            var mostrandoFind = cestaDeFrutas.Find(x => x.Cor == "Amarelo" || x.Cor == "Vermelho");
+
+            Console.WriteLine($"Id {mostrandoFind.Id} Nome {mostrandoFind.Nome}");
+
+            var mostrandoFindAll = cestaDeFrutas.FindAll(x => x.Cor == "Amarelo" || x.Cor == "Vermelho");
+
+            mostrandoFindAll.ForEach(i => Console.WriteLine($"Id {i.Id} Nome {i.Nome} Cor {i.Cor}"));
+
+            var listaOrdenada = mostrandoFindAll.OrderBy(x => x.Nome);
+
+            foreach (var item in listaOrdenada)
+            {
+                Console.WriteLine($"Nome {item.Nome}");
+            }
+
+            Console.WriteLine("-------------------------------------Find com order by");
+
+            var cestaDeFrutasFindOrder = cestaDeFrutas.OrderBy(x => x.Nome).ToList<Fruta>().Find(x => x.Cor == "Amarelo" || x.Cor == "Vermelho");
+
+            Console.WriteLine($"Id {cestaDeFrutasFindOrder.Id} Nome {cestaDeFrutasFindOrder.Nome}");
 
             Console.ReadKey();
         }
