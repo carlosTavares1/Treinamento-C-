26292cce550b486662608b9e511721cfbcc28852
Carlos Tavares <cast.3@hotmail.com>
2019-09-25 11:50:46 -0300

    Sistemas de vendas

---
+++ ExercicioAula13/BasePrincipal/Model/Veiculo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasePrincipal.Model
+{
+    public class Veiculo
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Valor { get; set; }
+        public int Quantidade { get; set; }
+        public DateTime DataDaVenda { get; set; }
+        public bool Ativo { get; set; }
+        public int UsuarioCriacao { get; set; }
+        public int UsuarioAlteracao { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public DateTime DataAlteracao { get; set; }
+    }
+}
---
+++ FuncoesLinq/SumLinq/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SomarInteiroLista();
+            SomarInteiroPrimitivos();
+            SomaBalasListaDeCriancas();
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// Este método realiza a soma de todas as posições do vetor
+        /// </summary>
+        private static void SomarInteiroPrimitivos()
+        {
+            //quando utilizamos [] em um tipo primitivo podemos criar uma coleção destes valores
+            int[] colecaoDeInteiros = new int[5] { 1, 2, 3, 4, 5 };
+            //aqui somamos o valor de todas as posições do vetor
+            Console.WriteLine(colecaoDeInteiros.Sum());
+        }
+        /// <summary>
+        /// Este método realiza a soma de todos os valores na lista e apresenta o resultado
+        /// </summary>
+        private static void SomarInteiroLista()
+        {
+            List<int> somarLista = new List<int>() {1,2,3,4,5 };
+            Console.WriteLine(somarLista.Sum());
+        }
+        /// <summary>
+        /// Este método faz a soma das balas da classe criança através de uma expressão lâmbida
+        /// </summary>
+        private static void SomaBalasListaDeCriancas()
+        {
+            List<Crianca> crianca = new List<Crianca>()
+            {
+                new Crianca()
+                {
+                    Nome = "Joazinho",
+                    Balas = 9
+                },
+
+                new Crianca()
+                {
+                    Nome = "Pedrinho",
+                    Balas = 68
+                }
+            };
+            Console.WriteLine(crianca.Sum(x => x.Balas));
+        }
+    }
+}
