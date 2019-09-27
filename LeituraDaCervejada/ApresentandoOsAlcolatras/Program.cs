c61829e845ecc0fa8e77fffae7348b0d61f48f8d
Carlos Tavares <cast.3@hotmail.com>
2019-09-27 17:25:27 -0300

    Criação de pastas, deleção e acesso à pasta

---
+++ LeituraDaCervejada/ApresentandoOsAlcolatras/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ListagemDeCervejas.Controller;
+using ListagemDeCervejas.Model;
+
+namespace ApresentandoOsAlcolatras
+{
+    class Program
+    {
+        static CervejaController cervejaController = new CervejaController();
+        static void Main(string[] args)
+        {
+            cervejaController.AdicionaNovaCerveja(new Cerveja()
+            {
+                Id = 6,
+                Nome = "Giomar",
+                Litros = 0.600,
+                Alcool = 8,
+                Valor = 0.98
+            });
+            cervejaController.MostraListaCerveja().ForEach(x => Console.WriteLine($"{x.Id} {x.Nome} {x.Litros} {x.Alcool} {x.Valor}"));
+            Console.WriteLine("Total litros " + cervejaController.RetornaSomaListaLitros());
+            Console.WriteLine("Total do valor " + cervejaController.RetornaSomaListaValor().ToString("C"));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ListagemDePastas/InterfaceListagem/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DllPastaWin;
+
+namespace InterfaceListagem
+{
+    class Program
+    {
+        static DocumentsAutoGenerate d = new DocumentsAutoGenerate();
+        static void Main(string[] args)
+        {
+            var menu = int.MinValue;
+
+            while(menu != 0)
+            {
+                Console.Clear();
+                Console.WriteLine("Sistema de pasta");
+                Console.WriteLine("1 - Criar Pasta em meus documentos");
+                Console.WriteLine("2 - Deletar pasta");
+                Console.WriteLine("3 - Exibir pasta");
+                Console.WriteLine("4 - Bonus sextooooooou");
+                Console.WriteLine("0 - sair");
+
+                menu = int.Parse(Console.ReadLine());
+
+                switch (menu)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Informe o nome da pasta:");
+                            var nomeDaPasta = Console.ReadLine();
+                            d.CriarPastaMeusDocumentos(nomeDaPasta);
+                        }
+                        break;
+                    case 2:
+                        {
+                            Console.WriteLine("Informe o nome da pasta que deseja apagar:");
+                            var nomeDaPasta2 = Console.ReadLine();
+                            d.DeletarPastaMeusDocumentos(nomeDaPasta2, true);
+                        }
+                        break;
+                    case 3:
+                        {
+                            Console.WriteLine("Pasta");
+                            d.ObterPastasDiretorioMeuDocumentos().ForEach(x => Console.WriteLine(x));
+                            Console.ReadKey();
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
