0ee13157ae2a216d7dac534ae4f5b975989ee70a
Carlos Tavares <cast.3@hotmail.com>
2019-10-01 08:43:41 -0300

    Organizando github

---
+++ AppCicle/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<string> conjunto = new SortedSet<string>();
+
+            conjunto.Add("jose");
+            conjunto.Add("gabriel");
+            conjunto.Add("carlos");
+
+            foreach (var item in conjunto)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
