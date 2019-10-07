b722decd768f68c7eeb01b65fef6c16f60996fd6
Carlos Tavares <cast.3@hotmail.com>
2019-10-07 16:46:15 -0300

    Exercicio de entity framework

---
+++ AlunosDoEntity/InterfaceDoEntity/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityClass.Controller;
+using EntityClass.Model;
+
+namespace InterfaceDoEntity
+{
+    class Program
+    {
+        static AlunoController aluno = new AlunoController();
+        static void Main(string[] args)
+        {
+            var opcao = int.MinValue;
+            while (opcao != 0)
+            {
+                Console.WriteLine("Informe o nome: ");
+                var nome = Console.ReadLine();
+                Console.WriteLine("Informe a idade: ");
+                var idade = int.Parse(Console.ReadLine());
+                Console.WriteLine("Digite zero para sair e ver a lista");
+                opcao = int.Parse(Console.ReadLine());
+                aluno.AddAluno(new Aluno()
+                {
+                    Nome = nome,
+                    Idade = idade
+                });
+            }
+
+            aluno.GetAlunos().ToList<Aluno>().ForEach(i => Console.WriteLine($"Nome: {i.Nome} Idade: {i.Idade}"));
+
+            Console.ReadKey();
+        }
+    }
+}
