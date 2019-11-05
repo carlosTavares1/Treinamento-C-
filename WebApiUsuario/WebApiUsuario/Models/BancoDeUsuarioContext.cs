e1f14e3d8943b58bc33d9ea2f4f8296ab5395ced
Carlos Tavares <cast.3@hotmail.com>
2019-11-05 17:57:43 -0200

    web api com swagger

---
+++ WebApiUsuario/WebApiUsuario/Models/BancoDeUsuarioContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebApiUsuario.Models
+{
+    public partial class BancoDeUsuarioContext : DbContext
+    {
+        public BancoDeUsuarioContext()
+        {
+        }
+
+        public BancoDeUsuarioContext(DbContextOptions<BancoDeUsuarioContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Usuario> Usuario { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuario>(entity =>
+            {
+                entity.Property(e => e.Ativo).HasDefaultValueSql("((1))");
+
+                entity.Property(e => e.DataAlteracao)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.DataCriacao)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(40);
+
+                entity.Property(e => e.Login)
+                    .IsRequired()
+                    .HasMaxLength(40);
+
+                entity.Property(e => e.Nome)
+                    .IsRequired()
+                    .HasMaxLength(40);
+
+                entity.Property(e => e.Senha)
+                    .IsRequired()
+                    .HasMaxLength(40);
+
+                entity.Property(e => e.UsuarioAlteracao).HasDefaultValueSql("((0))");
+
+                entity.Property(e => e.UsuarioCriacao).HasDefaultValueSql("((0))");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ WebApiUsuario/WebApiUsuario/Models/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApiUsuario.Models
+{
+    public partial class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+        public string Email { get; set; }
+        public bool? Ativo { get; set; }
+        public int? UsuarioCriacao { get; set; }
+        public int? UsuarioAlteracao { get; set; }
+        public DateTime? DataCriacao { get; set; }
+        public DateTime? DataAlteracao { get; set; }
+    }
+}
